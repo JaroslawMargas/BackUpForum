ee7d6b0a94d556b599ec99b72bef9d9f938e3731
BABA <jaromarg@gmail.com>
2018-11-02 12:44:03 -0700

    insert user
    insert user

--- src/SQLConnector.py
+++ src/SQLConnector.py
@@ -1,39 +1,75 @@
 import logging
 import mysql.connector
-import time
 import sys
 from mysql.connector import errorcode
 
 moduleLogger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-DB_NAME = 'name' 
+#DB_NAME = 'name' 
 
 TABLES = {}
-TABLES['index'] = (
-    "CREATE TABLE `index` ("
-    "  `id` int(11) NOT NULL AUTO_INCREMENT,"   #PK
-    "  `index` int(11) not NULL,"               #UNIQUE
-    "  `link` varchar(14) NOT NULL,"
-    "  PRIMARY KEY (`id`),UNIQUE KEY(`index`)"
+TABLES['main'] = (
+    "CREATE TABLE `main` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `number` varchar(10) not NULL,"
+    "  `link` varchar(255) NOT NULL,"
+    "  PRIMARY KEY (`id`)"
     ") ENGINE=InnoDB")
+
+TABLES['link'] = (
+    "CREATE TABLE `link` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `main_id` varchar(10) not NULL,"
+    "  `link` varchar(255) NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")
+
+TABLES['users'] = (
+    "CREATE TABLE `users` ("
+    "  `id` int(11) NOT NULL AUTO_INCREMENT,"
+    "  `name` varchar(255) NOT NULL,"
+    "  PRIMARY KEY (`id`),UNIQUE(`name`)"
+    ") ENGINE=InnoDB")
+
 
 class MySql(object):
 
-    def __init__(self,logger=None):
+    def __init__(self, logger=None, host=None, user=None, password=None, dbName = None):
         self.logger = logger or logging.getLogger(__name__)
-
-            
-    def ConnectToServer(self,hostInput,userInput,passwdInput):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.dbName = dbName
+        
+        self.mydB = None
+        self.cursor = None
+    
+    def dBConfiguration(self):
+        
+        try:
+            self.openSqlConnection()
+            self.deleteDatabase()
+            self.createDB()
+            self.setCursorExecutor()
+            self.createTable()
+            self.closeConnection()
+        except:
+            self.logger.debug("Error while Database configuration")
+        else:
+            self.logger.debug("Database OK")
+           
+    def openSqlConnection(self):
         self.logger.info('Connection initialization')
     
         try:
-            mydb = mysql.connector.connect(
-                host=hostInput,
-                user=userInput,
-                passwd=passwdInput
+            mydB = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                passwd=self.password
                 )
-            return mydb
+            self.mydB = mydB
+            self.createCursorExecutor()
             
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
@@ -42,52 +78,43 @@ class MySql(object):
                 self.logger.debug("Database does not exist: {}".format(err))
             else:
                 self.logger.debug(err)
-            sys.exit("Closed connection")
-   
-    def CreateCursorExecutor(self,mydb):
-        mycursor = mydb.cursor()
-        return mycursor
+            #sys.exit("Closed connection")
     
-    def SetCursorExecutor(self,mycursor,dBName):
-        mycursor.execute("USE {}".format(dBName))
-        return mycursor
+    def closeConnection(self):
+        self.mydB.close()
+        self.cursor.close()
+        
+    def createCursorExecutor(self):
+        self.cursor = self.mydB.cursor()
+    
+    def setCursorExecutor(self):
+        self.cursor.execute("USE {}".format(self.dbName))
 
-    def CreateDB(self,mycursor,dBName):
+    def createDB(self):
         try:
-            mycursor.execute(
-            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(dBName))
+            self.cursor.execute(
+            "CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(self.dbName))
             self.logger.info('Database created')
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_DB_CREATE_EXISTS:
                 self.logger.debug("Failed create database: {}".format(err))
                 
         
-    def DeleteDatabase(self,mycursor,dBName):
+    def deleteDatabase(self):
         try:
-            mycursor.execute("DROP DATABASE "+dBName)
+            self.cursor.execute("DROP DATABASE "+self.dbName)
             self.logger.info('Database dropped/deleted')
         except mysql.connector.Error as err:
             if err.errno == errorcode.ER_DB_DROP_EXISTS:
                 self.logger.debug("Failed drop database: {}".format(err))
             elif err.errno == errorcode.ER_DB_DROP_RMDIR:
                 self.logger.debugg()
-                
-    def ConnectToDB(self,mydb,dBName):
-        try:
-            mydb.connect(database=dBName)
-            self.logger.info("Connected to: "+dBName)
-        except mysql.connector.Error as err:
-            self.logger.debug(err)
-            
-            
-    def CloseDB(self,mydb):
-        mydb.close()
-    
-    def CreateTable(self,mycursor):
+
+    def createTable(self):
         for tableName in TABLES:
             tableDescription = TABLES[tableName]
             try:
-                mycursor.execute(tableDescription)
+                self.cursor.execute(tableDescription)
                 self.logger.info("Create table: "+tableName)
             except mysql.connector.Error as err:
                 if err.errno == errorcode.ER_TABLE_EXISTS_ERROR:
@@ -95,15 +122,37 @@ class MySql(object):
                 else:
                     self.logger.debug(format(err))
 
-
-# test 
-sql = MySql()
-mydb = sql.ConnectToServer('localhost','root','rootpassword') 
-cursor = sql.CreateCursorExecutor(mydb)
-sql.CreateDB(cursor,DB_NAME)
-cursor = sql.SetCursorExecutor(cursor,DB_NAME)
-#sql.DeleteDatabase(cursor,DB_NAME)
-sql.CreateTable(cursor)
-#sql.ConnectToDB(mydb,DB_NAME)
-#sql.CloseDB(mydb)
-
+    def showTables(self):
+        self.cursor.execute("SHOW TABLES")
+        tablesList = [self.cursor.fetchall()] 
+        return tablesList
+    
+    def getColumnsNotIncrement(self, dBTable):
+        self.cursor.execute("SHOW COLUMNS FROM "+self.dbName+"."+dBTable+" WHERE EXTRA NOT LIKE '%auto_increment%'")
+        columnLst = [column[0] for column in self.cursor.fetchall()]
+        return columnLst
+    
+    def insertLink(self, tableName, valList):
+        columnInTableList = self.getColumnsNotIncrement(tableName)
+        colNames = ''
+        for x in range(columnInTableList.__len__()): #
+            colNames +=columnInTableList[x]+","
+        try:
+            sql = "INSERT INTO "+tableName+" ("+colNames[0:-1]+") VALUES (%s, %s)" #remove last ","
+            self.cursor.execute(sql, valList)
+            self.mydB.commit()
+            self.logger.info('Data are inserted and committed')
+        except mysql.connector.Error as err:
+            self.logger.debug(err)
+            
+    def insertUser(self, tableName, valueList):
+        columnInTableList = self.getColumnsNotIncrement(tableName)
+        try:
+            #sql = "INSERT INTO "+tableName+" ("+str(columnInTableList[0])+") VALUES (%s)" #remove last ","
+            #tmpList = [value]
+            sql = "INSERT IGNORE INTO users ("+columnInTableList[0]+") VALUES (%s)"
+            self.cursor.execute(sql,valueList)
+            self.mydB.commit()
+            self.logger.info('Data are inserted and committed')
+        except mysql.connector.Error as err:
+            self.logger.debug(err)
--- src/sideParser.py
+++ src/sideParser.py
@@ -1,33 +1,43 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import logging
 import mechanize
 from bs4 import BeautifulSoup as BS
 import fileStream
 import numberConverter
 import sys
+import SQLConnector
 
 str_nextLink = "Następny"
 str_advertisement = "Reklama"
 str_logOut = "Wyloguj"
 
+moduleLogger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
 
 class SideParser():
 
-    def __init__ (self):
+    def __init__ (self,logger=None):
+        self.logger = logger or logging.getLogger(__name__)
         self.br = mechanize.Browser()
-        self.m_count  = 0  #counter for files.
-        self.m_nextLink =""  #link for new page 
-        self.m_logOutUser = ""  
-
+        self.fileCount  = 0  #counter for files.
+        self.id = 0 #counter for id in file
+        self.nextLink =""  #link for new page 
+        self.logOutUser = ""
+        
+        self.sql = SQLConnector.MySql(host='localhost',user='username',password='userpassword',dbName = 'fora')
+        self.sql.dBConfiguration()
+            
     #need to ignore the robots.txt
-    def ignore_robots(self):
+    def ignoreRobots(self):
         self.br.set_handle_robots(False)
         
-    def open_browser(self,browseAdres):    
+    def openBrowser(self,browseAdres):    
         self.br.open(browseAdres)
         #print all forms
         for f in self.br.forms():
-                print f
+                self.logger.info(format(f))
+        return True
     
     # example form
     # <form action="login.php" method="post" target="_top">
@@ -35,16 +45,17 @@ class SideParser():
 
     #find and select atr:action="login.php"
     def selectForm(self,attribute,atrValue):
-        try:
-            self.br.select_form(predicate=lambda frm: attribute in frm.attrs and frm.attrs[attribute] == atrValue)
-        except:
-            sys.exit("Selecting problem.")
+        if(self.br.select_form(predicate=lambda frm: attribute in frm.attrs and frm.attrs[attribute] == atrValue)):
+            self.logger.debug("Error Form Selection")
+            return True
+        else:
+            self.logger.info("Form Selected")
  
     #if form is selected, then enter user and  password
-    def log_user(self):
+    def logUser(self):
         userInput = str(raw_input("Please enter user to continue: "))
         self.br.form['username'] = userInput #userInput
-        self.m_logOutUser = userInput
+        self.logOutUser = userInput
         password = str(raw_input("Please enter password to continue: "))
         self.br.form['password'] = password #password
         self.br.submit()
@@ -52,56 +63,84 @@ class SideParser():
     # Create TXT file with fileName (increase name ++) 
     # Open browser and read all links with attribute and value
     # Append each link to file
-    def readLinks_SaveToFile(self,fileName,linkAtrName,linkAtrValue):
+    def readLinks_SaveToFile(self,fileName,linkAtrName,linkAtrValue,SQLtableName):
         streamFile = fileStream.FileStream()
-        fileNameExt = streamFile.createTxtFile(fileName)
+        fileNameExt = streamFile.createTxtFile("Link_"+fileName)
         
+#        self.fileCount = 1
         numberLeadZero = numberConverter.NumberConverter()
+        try:
+            self.sql.openSqlConnection()
+            self.sql.setCursorExecutor()
+        except:
+            self.logger.debug("SQL Connection error.")
+        
         for link in self.br.links():     
             for name,value in link.attrs:
                 if name == linkAtrName and value == linkAtrValue:
-                    idLeading = numberLeadZero.toLeadingZero(self.m_count)
+                    idLeading = numberLeadZero.toLeadingZero(self.id)
+                    self.logger.info("ReadLinks: {}".format(link.url))
                     streamFile.appendString(fileNameExt,idLeading+"|"+link.url)
-                    self.m_count += 1
+                    self.sql.insertLink(SQLtableName,[fileName,link.url])
+                    self.id += 1
         del streamFile,numberLeadZero
+        try:
+            self.sql.closeConnection()
+        except:
+            self.logger.debug("SQL Close connection error.")
+        
+        
    
-    def getCounter(self):
-        return self.m_count
+    def getFileCounter(self):
+        return self.fileCount
     
     def printCurrentHtml(self):
         g_response= self.br.response()
         soup = BS(g_response)  #BeautifulSoup
-        print soup
+        self.logger.info(format(soup))
         
     def printCurrentResponse(self):
         g_response= self.br.response()
-        print g_response
+        self.logger.info("RESPONSE: {}".format(g_response))
 
            
     def logOut(self):
-        target_text=str_logOut+' [ '+self.m_logOutUser+' ][IMG]'+str_logOut+' [ '+self.m_logOutUser+' ]'
+        target_text=str_logOut+' [ '+self.logOutUser+' ][IMG]'+str_logOut+' [ '+self.logOutUser+' ]'
         for link in self.br.links():
             if link.text == target_text:
-                print('The user is log out')           
+#                 self.logger.info("The user is log out")        
                 break
-        self.br.follow_link(link)
-        print(self.br.geturl())
+            
+        if(self.br.follow_link(link)):
+            self.logger.info("User log out")
+        else:
+            self.logger.debug("User log out error")
+            
+                
+        self.logger.info("URL: {}".format(self.br.geturl()))
     
     # Create TXT file with name INDEX and read main links. Save it in file.
     # Open INDEX file and read line by line links.
     # Open each link and read POSTED links. Save it in ID++ file.
     # Link maps is created.
-    def createLinkMap(self,filename,artibuteName,artibuteValue):
-        self.readLinks_SaveToFile('index','class','forumlink')
-        with open(filename, "r") as f:
+    def createLinkMap(self):
+        
+        self.fileCount = 0
+        self.id = 1
+        numberLeadZero = numberConverter.NumberConverter()
+        idLeading = numberLeadZero.toLeadingZero(self.fileCount)
+        self.readLinks_SaveToFile(idLeading,'class','forumlink','main')
+        
+        self.id = 1
+        with open("Link_"+idLeading+".txt", "r") as f:
             for line in f:
                 str_line = str(line)
-                print str_line
+                #print str_line
                 idLink,link = str_line.split("|")
-                print idLink
-                print link
+                #print idLink
+                #print link
                 self.br.open(link)
-                self.readLinks_SaveToFile(idLink,artibuteName,artibuteValue)     
+                self.readLinks_SaveToFile(idLink,'class','topictitle','link')     
         f.close()
                   
     # Check if Post has next page.
@@ -113,7 +152,7 @@ class SideParser():
         for post in soup.findAll("span", {"class": "nav"}):
             for a in post.findAll('a'):
                 if(a.get_text().encode('utf-8') == str_nextLink):
-                    self.m_nextLink = a.get('href')
+                    self.nextLink = a.get('href')
                     checker = True
         if (checker == None):
             return True
@@ -123,48 +162,60 @@ class SideParser():
     # Save all users 
     # Save all Posts
     # If next page exist repeat it
-    def readPost_CreateFile(self,idLink):
-            with open(idLink+".txt", "r") as fl:
-                        streamFile = fileStream.FileStream()
-                        for line in fl:
-                            str_line = str(line)
-                            print str_line
-                            idLink,link = str_line.split("|")
-                            print idLink
-                            print link   
-            
-                            self.m_nextLink = link 
-                            fileName = streamFile.createTxtFile(idLink)
-                            while True:
-                                self.br.open(self.m_nextLink)
-                                #open link and read POST
-                                g_response= self.br.response()  #set current response from side.
-                                soup = BS(g_response,"html5lib")  #set instance BeautifulSoup
-                                 
-                                for user in soup.findAll("span", {"class": "name"}):
-                                    if(user.b):
-                                        if(user.get_text().encode('utf-8') != str_advertisement):
-                                            streamFile.appendString(fileName,"Users: "+user.get_text().encode('utf-8'))
-                                for post in soup.findAll("span", {"class": "postbody"}):
-                                    #if the post is a script, ignore it
-                                    if(post.script):
-                                            continue
-                                    else:
-                                        streamFile.appendString(fileName,"Posts: "+post.get_text().encode('utf-8'))
+    def readPost_CreateFile(self,idLinkName):
+        
+        self.sql.openSqlConnection()
+        self.sql.setCursorExecutor()
+
+
+        with open("Link_"+idLinkName+".txt", "r") as fl:
+                    streamFile = fileStream.FileStream()
+                    for line in fl:
+                        str_line = str(line)
+                        #print str_line
+                        idLink,link = str_line.split("|")
+                        self.logger.info("Id Link: {}".format(idLink)) 
+                        self.logger.info("Link: {}".format(link))     
+                        self.nextLink = link 
+                        fileName = streamFile.createTxtFile("Post_"+idLink)
+                        while True:
+                            self.br.open(self.nextLink)
+                            #open link and read POST
+                            g_response= self.br.response()  #set current response from side.
+                            soup = BS(g_response,"html5lib")  #set instance BeautifulSoup
+                             
+                            for user in soup.findAll("span", {"class": "name"}):
+                                if(user.b):
+                                    if(user.get_text().encode('utf-8') != str_advertisement):
+                                        streamFile.appendString(fileName,"Users: "+user.get_text().encode('utf-8'))
                                         
-                                if(self.checkNextPage(self.m_nextLink)):
-                                    break
-            fl.close()
+                                        self.sql.insertUser("users",[user.get_text().encode('utf-8')])
+                                                  
+                            for post in soup.findAll("span", {"class": "postbody"}):
+                                #if the post is a script, ignore it
+                                if(post.script):
+                                        continue
+                                else:
+                                    streamFile.appendString(fileName,"Posts: "+post.get_text().encode('utf-8'))
+                                    
+                            if(self.checkNextPage(self.nextLink)):
+                                break
+        fl.close()
+        self.sql.closeConnection()
+        self.logger.debug("SQL Connection error")
           
     # create a posts map.
-    def createPostsMap(self,filename):
-        with open(filename, "r") as f:
-            for line in f:
+    def createPostsMap(self):
+        self.fileCount = 0
+        numberLeadZero = numberConverter.NumberConverter()
+        fileNameLeading = numberLeadZero.toLeadingZero(self.fileCount)
+        with open("Link_"+fileNameLeading+".txt", "r") as fl:   #open Link_000.txt
+            for line in fl:
                 str_line = str(line)
-                print str_line
+                #print str_line
                 idLink,link = str_line.split("|")
-                print idLink
-                print link
+                self.logger.info("Id Link: {}".format(idLink)) #001
+                self.logger.info("Link: {}".format(link))  #link
                 self.readPost_CreateFile(idLink)
                 
-        f.close()
+        fl.close()
